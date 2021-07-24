ce86902538f5190ea02938d3fe4831b675ce5934
gwon713 <62048410+gwon713@users.noreply.github.com>
2021-07-25 02:32:36 +0900

    변경 : decribe , it summary 수정

--- algorism_test_mocha/test/test.spec.js
+++ algorism_test_mocha/test/test.spec.js
@@ -3,17 +3,19 @@ import assert from "assert";
 
 const testCase = [
     { brown:10, yellow:2, expected_value:[4, 3] },
-    { brown:8, yellow:1, expected_value:[3, 3] },
+    { brown:8, yellow:1, expected_value:[3, 2] },
     { brown:24, yellow:24, expected_value:[8, 6] }
 ];
 
-describe("실행 결과", () => {
-    testCase.forEach(({brown, yellow, expected_value}) => {
+describe("test.js 알고리즘 실행 결과", () => {
+    testCase.forEach(({brown, yellow, expected_value},index) => {
         const result = JSON.stringify(solution(brown, yellow));
         const expect = JSON.stringify(expected_value)
-        const msg = result === expect ? "Successful" : "Failed"
-        it(msg, () => {
+        const check = (result, expect)=>{
             assert.strictEqual(result, expect);
+        }
+        it("test_case "+index, () => {
+            check(result,expect);
         })
     });
 });
