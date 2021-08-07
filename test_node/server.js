bb5f2678fe7d28bdaadf0e535d4993d8cca57448
Gwon Min Je <gwon713@gmail.com>
2021-08-07 19:22:18 +0900

    Update server.js

--- test_node/server.js
+++ test_node/server.js
@@ -1,6 +1,5 @@
 const express = require('express'); 
-const path = require('path')// 파일 경로 모듈
-require('dotenv').config({ path: path.join(__dirname, './env/server.env') });//env 로드 모듈
+require('dotenv').config('./env/server.env');//env 로드 모듈
 
 const app = express(); 
 
@@ -9,10 +8,13 @@ app.use(express.urlencoded({extended: true}));//bodyparser 설정
 
 const port = process.env.PORT || 5000; 
 
-app.get('/test', (req, res, next) => {
+app.get('/test', (req, res) => {
     res.status(200).send({
         message : "test!"
     })
+});
+app.post('/test', (req, res) => {
+
 });
 
 
